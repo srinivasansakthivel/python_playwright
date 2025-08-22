e80673291ea495895b6759868cf0f7c651bdb4b1
Srinivasan Sakthivel <starsrini5@gmail.com>
2025-08-22 08:39:59 +0530

    Commiting the snapshot related tests

---
+++ visual_tests/test_landing_page_visual_2.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import expect
+from pytest_playwright_visual.plugin import assert_snapshot
+
+from pom.home_page_elements import HomePage
+
+
+def test_visual_landing_2_1(page, assert_snapshot) -> None:
+    #Assess - Given
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    homepage = HomePage(page)
+    # expect(homepage.celebrate_body).to_be_visible()
+    assert_snapshot(page.screenshot())
+
+
+def test_visual_landing_2_2(page, assert_snapshot) -> None:
+    #Assess - Given
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    homepage = HomePage(page)
+    # expect(homepage.celebrate_body).to_be_visible()
+    assert_snapshot(page.screenshot())
